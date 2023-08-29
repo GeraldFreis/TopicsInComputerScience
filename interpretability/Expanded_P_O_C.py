512d6cac02bb16d9abc2b00d94d7a6afec3e3bfb
Gerald Freislich <freislichg@gmail.com>
2023-08-29 13:18:10 +0930

    refactoring

--- interpretability/Expanded_P_O_C.py
+++ interpretability/Expanded_P_O_C.py
@@ -6,6 +6,21 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 CNN = keras.models.load_model("../CNN_Non_Dense")
 raw_data = pd.read_csv("IMDB.csv")
 first_paragraph = raw_data.review[0]
+
+"""Function to automate tokenizing and padding"""
+def tokenization(string_to_padd_and_tok: str, tokenizer):
+    tokenized = tokenizer.texts_to_sequences(string_to_padd_and_tok)
+    padded = tf.keras.utils.pad_sequences(tokenized, maxlen=1000, padding="post")
+    return padded
+
+"""Function to convert sentence list into strings"""
+def stringify(sentence_list: list, lower_bound: int, upper_bound: int)->str:
+    sentences_in_bounds = [sentence_list[i] for i in range(lower_bound, upper_bound)]
+    sub_para = str()
+    for sentence in sentences_in_bounds:
+        sub_para += sentence + "."
+    return sub_para
+
 """
 Tree_Creator takes a paragraph and a max_depth and returns a list of CNN's prediction for each recursive subsection of the paragraph
 """
@@ -16,26 +31,16 @@ def Tree_Creator(paragraph: str, max_depth: int, current_layer: int, main_list: 
 
     if(len(sentences) <= 1 or max_depth==current_layer): return main_list
 
-    # computing each layer of sentences
-    A = [sentences[i] for i in range(0, int(len(sentences)/2))]
-    B = [sentences[i] for i in range(int(len(sentences)/2), len(sentences))]
+    # getting the subsets of the paragraph    
+    A = stringify(sentences, 0, int(len(sentences)/2))
+    B = stringify(sentences, int(len(sentences)/2), len(sentences))
 
-    string_A = str()
-    string_B = str()
+    tokenizer = Tokenizer(num_words=max(len(A), len(B)))
+    tokenizer.fit_on_texts(A)
 
-    for sentence in A:
-        string_A += sentence + "."
-    for sentence in B:
-        string_B += sentence + "."
-
-    tokenizer = Tokenizer(num_words=max(len(string_A), len(string_B)))
-    tokenizer.fit_on_texts(string_A)
-    tokenized_A = tokenizer.texts_to_sequences(string_A)
-    tokenized_B = tokenizer.texts_to_sequences(string_B)
-
-    # padding the texts
-    padded_A = tf.keras.utils.pad_sequences(tokenized_A, maxlen=1000, padding="post")
-    padded_B = tf.keras.utils.pad_sequences(tokenized_B, maxlen=1000, padding="post")
+    # padding the texts and tokenizing
+    padded_A = tokenization(A, tokenizer)
+    padded_B = tokenization(B, tokenizer)
 
     # loading the CNN
     model = keras.models.load_model("../CNN_Non_Dense")
@@ -47,12 +52,13 @@ def Tree_Creator(paragraph: str, max_depth: int, current_layer: int, main_list: 
     avg_A = float(sum(prediction_A)) / float(len(prediction_A))
     avg_B = float(sum(prediction_B)) / float(len(prediction_B)) 
 
-    layer = {"Layer": current_layer, "A": string_A, "Prediction_A": avg_A, "B": string_B, "Prediction_B": avg_B}
+    layer = {"Layer": current_layer, "A": A, "Prediction_A": avg_A, "B": B, "Prediction_B": avg_B}
     main_list.append(layer)
 
-    main_list = Tree_Creator(string_A, max_depth, current_layer+1, main_list)
-    main_list = Tree_Creator(string_B, max_depth, current_layer+1, main_list)
+    # recursively getting the next layers
+    main_list = Tree_Creator(A, max_depth, current_layer+1, main_list)
+    main_list = Tree_Creator(B, max_depth, current_layer+1, main_list)
     
     return main_list
 
-print(Tree_Creator(first_paragraph, 7, 1, list()))
+print(Tree_Creator(first_paragraph, 2, 1, list()))
